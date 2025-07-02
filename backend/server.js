10562fa5cf99ae5857ad7d5707a9ca9db28bfa8a
ThulyaRodrigo <thulyahansani2002@gmail.com>
2025-07-02 12:42:07 +0530

    Backend and Frontend initialization

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // Parse JSON bodies
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Todo App Backend is Running!");
+});
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error(err));
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
